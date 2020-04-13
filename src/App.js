e7948e144780e72f2979f15c5120ae49208094a4
Yamatos <sqyamatos@gmail.com>
2020-04-13 18:05:20 +0700

    Up for change computer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import MovieLists from './components/MovieLists';
 
 const API_URL = 'https://api.themoviedb.org/3/search/movie';
 const API_KEY = 'd2865b1433557a7176ac697d7aba876f';
 
 function App() {
-	const [itemsContrainer, setItemsContrainer] = useState({ results: [] });
-	const [query, setQuery] = useState('a');
+  const [itemsContrainer, setItemsContrainer] = useState({ results: [] });
+  const [query, setQuery] = useState('a');
 
-	useEffect(() => {
-		if (query === '') {
-			return;
-		}
-		axios
-			.get(API_URL, { params: { api_key: API_KEY, query: query } })
-			.then((response) => {
-				console.log(response.data);
-				setItemsContrainer(response.data);
-			})
-			.catch((error) => {
-				console.log('error ' + error);
-			});
-	}, [query]);
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    axios
+      .get(API_URL, { params: { api_key: API_KEY, query: query } })
+      .then((response) => {
+        console.log(response.data);
+        setItemsContrainer(response.data);
+      })
+      .catch((error) => {
+        console.log('error ' + error);
+      });
+  }, [query]);
 
-	return (
-		<div className="App">
-			<input value={query} onChange={(e) => setQuery(e.target.value)} />
-			<MovieLists movies={itemsContrainer.results} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+      <MovieLists movies={itemsContrainer.results} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MovieLists.js
+++ src/components/MovieLists.js
@@ -1,19 +1,47 @@
 import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+    slidesToSlide: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    slidesToSlide: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export default function MovieLists({ movies }) {
-	const movieLists = movies.map((movie) => {
-		return (
-			<div className="todolist" key={movie.id}>
-				<div>
-					<div id="title">{movie.title}</div>
-					<div>{movie.overview}</div>
-					<img
-						src={'https://image.tmdb.org/t/p/w500/' + movie.poster_path}
-						alt={movie.title + ' poster'}
-					/>
-				</div>
-			</div>
-		);
-	});
-	return <div className="todoContrainer">{movieLists}</div>;
+  const movieLists = movies.map((movie) => {
+    return (
+      <div className="todolist" key={movie.id}>
+        <div>
+          <div id="title">{movie.title}</div>
+          <div>{movie.overview}</div>
+          <img
+            src={'https://image.tmdb.org/t/p/w92/' + movie.poster_path}
+            alt={movie.title + ' poster'}
+          />
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div className="todoContrainer">
+      <Carousel responsive={responsive}>{movieLists}</Carousel>
+    </div>
+  );
 }
