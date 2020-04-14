357412772610b293ca780ba7f175352f8e1a5e6e
Yamatos <sqyamatos@gmail.com>
2020-04-14 19:05:46 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.scss';
+import MovieDetail from './components/MovieDetail';
 import MovieLists from './components/MovieLists';
 
-const API_URL = 'https://api.themoviedb.org/3/search/movie';
+const API_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'd2865b1433557a7176ac697d7aba876f';
 
 function App() {
   const [itemsContrainer, setItemsContrainer] = useState({ results: [] });
   const [query, setQuery] = useState('a');
+  const [movie, setMovie] = useState(null);
+  const [genreList, setGenreList] = useState([]);
 
   useEffect(() => {
     if (query === '') {
       return;
     }
     axios
-      .get(API_URL, { params: { api_key: API_KEY, query: query } })
+      .get(API_URL + '/search/movie', {
+        params: { api_key: API_KEY, query: query },
+      })
       .then((response) => {
         console.log(response.data);
         setItemsContrainer(response.data);
+        setMovie(response.data.results[0]);
       })
       .catch((error) => {
         console.log('error ' + error);
       });
   }, [query]);
 
+  useEffect(() => {
+    axios
+      .get(API_URL + '/genre/movie/list', {
+        params: { api_key: API_KEY, language: 'en-US' },
+      })
+      .then((response) => {
+        setGenreList(response.data.genres);
+      })
+      .catch((error) => {
+        console.log('error ' + error);
+      });
+  }, [genreList.length]);
+
+  const handleMovieClick = (movie) => {
+    setMovie(movie);
+  };
+
   return (
     <div className="App">
-      <input value={query} onChange={(e) => setQuery(e.target.value)} />
-      <MovieLists movies={itemsContrainer.results} />
+      <input onChange={(e) => setQuery(e.target.value)} />
+      {movie && <MovieDetail movie={movie} genreList={genreList} />}
+      <MovieLists
+        movies={itemsContrainer.results}
+        handleMovieClick={handleMovieClick}
+      />
     </div>
   );
 }
--- src/components/MovieLists.js
+++ src/components/MovieLists.js
@@ -1,47 +1,54 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import './MovieLists.scss';
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-    slidesToSlide: 5,
+    items: 10,
+    slidesToSlide: 9,
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 990 },
+    items: 5,
+    slidesToSlide: 4,
+  },
+  tablet: {
+    breakpoint: { max: 990, min: 765 },
     items: 3,
     slidesToSlide: 3,
   },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2,
-  },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 765, min: 430 },
+    items: 2,
+  },
+  mobile2: {
+    breakpoint: { max: 430, min: 0 },
     items: 1,
   },
 };
 
-export default function MovieLists({ movies }) {
+export default function MovieLists({ movies, handleMovieClick }) {
   const movieLists = movies.map((movie) => {
     return (
-      <div className="todolist" key={movie.id}>
-        <div>
-          <div id="title">{movie.title}</div>
-          <div>{movie.overview}</div>
-          <img
-            src={'https://image.tmdb.org/t/p/w92/' + movie.poster_path}
-            alt={movie.title + ' poster'}
-          />
-        </div>
+      <div key={movie.id}>
+        <img
+          src={'https://image.tmdb.org/t/p/w154/' + movie.poster_path}
+          alt={movie.title + ' poster'}
+          onClick={() => handleMovieClick(movie)}
+        />
       </div>
     );
   });
   return (
-    <div className="todoContrainer">
-      <Carousel responsive={responsive}>{movieLists}</Carousel>
-    </div>
+    <Container className="movie-contrainer">
+      <Row>
+        <Col>
+          <Carousel responsive={responsive}>{movieLists}</Carousel>
+        </Col>
+      </Row>
+    </Container>
   );
 }
